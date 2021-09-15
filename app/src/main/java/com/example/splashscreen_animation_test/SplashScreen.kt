13dac378fa902470d5f5423288115f6ab4932301
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-15 13:47:19 +0900

    [Feature] - Change the order of displaying background

--- app/src/main/java/com/example/splashscreen_animation_test/SplashScreen.kt
+++ app/src/main/java/com/example/splashscreen_animation_test/SplashScreen.kt
@@ -1,5 +1,17 @@
 package com.example.splashscreen_animation_test
 
-class SplashScreen{
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        val topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animaion)
+
+    }
 }
